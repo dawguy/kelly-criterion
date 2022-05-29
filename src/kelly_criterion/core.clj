d4269c60e005a2213951cbb0970cfa50238694c9
David Wright <hockeyguy411@gmail.com>
2022-05-29 13:35:52 -0400

    Adds growth rate calculations from formula.
    Formula essentially says something along the lines of.
    
    sum(Prob i * log(money not bet + money bet + return of bet on Prob i))

--- src/kelly_criterion/core.clj
+++ src/kelly_criterion/core.clj
@@ -19,14 +19,36 @@
 
 (defn bet-to-odds [payout] (/ payout 100))
 
+(defn normalize-probs [chances]
+  (let [prob-sum (reduce #(+ %1 (:prob %2)) 0 chances)] (map #(assoc %1 :prob (/ (:prob %1) prob-sum)) chances)))
+
 (defn expected-value [chances]
-  (reduce + 0 (map #(* (:prob %) (:payout %)) chances))
+  (reduce + 0 (map #(* (:prob %) (/ (:payout %) 100)) chances))
 )
+
+(defn translate-to-bet-payoff [chances]
+  (map #(array-map :prob (:prob %) :payout (- (/ (:payout %1) 100) 1)) chances))
 
 ; I don't have the formula for this memorized :'(
 ; Going to simulate it based on averaging a couple thousand runs of a couple thousand bets
 (defn growth-rate [chances bet-size]
+  ; https://math.stackexchange.com/questions/662104/kelly-criterion-with-more-than-two-outcomes
+  ; sum(p * (log(1 + odds * bet-size) + log(M))
+  (reduce + 0 (map #(* (:prob %) (Math/log (+ 1 (* (:payout %) bet-size)))) (translate-to-bet-payoff (normalize-probs chances))))
   )
+
+(defn find-growth-rates [chances] (map #(vector %1 (growth-rate chances (/ %1 100))) (range 101)))
+
+(defn optimal-growth-rate [chances]
+  "Calculates the optimal whole percentage growth rate based on a given set of probabilities."
+  (let [v (find-growth-rates chances)]
+    (loop [a (first v)
+           b (second v)
+           rem (rest (rest v))]
+      (if (< (second a) (second b))
+        (recur b (first rem) (rest rem))
+        a))
+    ))
 
 (defn simulate-bet [chances bet-amount]
   (loop [dart (rand)
@@ -59,8 +81,11 @@
 (comment
 "A list of runnable helpful commands"
 []
-  (prn "yo dog")
+  (def bet-size 0.1)
+  (def chances [{:prob 0.6 :payout 200} {:prob 0.4 :payout 0}])
+  (def chances [{:prob 0.6 :payout 200} {:prob 0.4 :payout 0.25}])
   (def chances [{:prob 0.15 :payout 300} {:prob 0.35 :payout 200} {:prob 0.5 :payout 0}])
+  (def chances [{:prob 0.3 :payout 300} {:prob 0.7 :payout 200} {:prob 1.0 :payout 0}])
   (def chances [{:prob 0.1 :payout 1250} {:prob 0.9 :payout 0}])
   (def chances [{:prob 0.025 :payout 1500} {:prob 0.1 :payout 500} {:prob 0.2 :payout 300} {:prob 0.15 :payout 150} {:prob 0.35 :payout 50} {:prob 0.175 :payout 20}])
   (def chances [{:prob 0.02 :payout 2000} {:prob 0.03 :payout 1500} {:prob 0.05 :payout 1000} {:prob 0.05 :payout 500} {:prob 0.25 :payout 250} {:prob 0.15 :payout 150} {:prob 0.45 :payout 25}])
